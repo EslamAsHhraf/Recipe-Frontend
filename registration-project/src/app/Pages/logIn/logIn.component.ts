32a6a08c535d1a286f844d260cae5f406ad6d283
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-08-29 16:30:23 +0300

    add folder structure

---
+++ registration-project/src/app/Pages/logIn/logIn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logIn',
+  templateUrl: './logIn.component.html',
+  styleUrls: ['./logIn.component.css']
+})
+export class LogInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- registration-project/src/app/app.module.ts
+++ registration-project/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './Pages/register/register.component';
+import { LogInComponent } from './Pages/logIn/logIn.component';
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LogInComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, RegistrationComponent, RegisterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PanelModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
