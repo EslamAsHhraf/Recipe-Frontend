6cd34b6462518b7500675fbc7807883bd9550373
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-09-08 19:28:28 +0300

    refactor files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
-import { ingredientsComponent } from './demo/components/ingredients/ingredients.component';
-import { recipeComponent } from './demo/components/recipe/recipe.component';
 
 @NgModule({
     imports: [
@@ -28,32 +26,11 @@ import { recipeComponent } from './demo/components/recipe/recipe.component';
                                 ).then((m) => m.DashboardModule),
                         },
                         {
-                            path: 'uikit',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/uikit/uikit.module'
-                                ).then((m) => m.UIkitModule),
-                        },
-                        {
                             path: 'utilities',
                             loadChildren: () =>
                                 import(
                                     './demo/components/utilities/utilities.module'
                                 ).then((m) => m.UtilitiesModule),
-                        },
-                        {
-                            path: 'documentation',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/documentation/documentation.module'
-                                ).then((m) => m.DocumentationModule),
-                        },
-                        {
-                            path: 'blocks',
-                            loadChildren: () =>
-                                import(
-                                    './demo/components/primeblocks/primeblocks.module'
-                                ).then((m) => m.PrimeBlocksModule),
                         },
                         {
                             path: 'pages',
@@ -90,13 +67,6 @@ import { recipeComponent } from './demo/components/recipe/recipe.component';
                     loadChildren: () =>
                         import('./demo/components/auth/auth.module').then(
                             (m) => m.AuthModule
-                        ),
-                },
-                {
-                    path: 'landing',
-                    loadChildren: () =>
-                        import('./demo/components/landing/landing.module').then(
-                            (m) => m.LandingModule
                         ),
                 },
                 { path: 'notfound', component: NotfoundComponent },
--- src/app/demo/components/pages/pages-routing.module.ts
+++ src/app/demo/components/pages/pages-routing.module.ts
@@ -3,9 +3,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: 'crud', loadChildren: () => import('./crud/crud.module').then(m => m.CrudModule) },
-        { path: 'empty', loadChildren: () => import('./empty/emptydemo.module').then(m => m.EmptyDemoModule) },
-        { path: 'timeline', loadChildren: () => import('./timeline/timelinedemo.module').then(m => m.TimelineDemoModule) },
         { path: 'recipe',  loadChildren: () => import('./recipe/recipe.module').then(m => m.recipeModule) },
         { path: '**', redirectTo: '/notfound' }
     ])],
