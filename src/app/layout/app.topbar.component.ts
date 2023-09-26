999a7d3aac8446f8621be24d0d39d8badcccdbda
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-09-26 23:20:27 +0300

    add icon on top menu

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { LayoutService } from "./service/app.layout.service";
+import { LayoutService } from './service/app.layout.service';
 import { Recipe } from '../model/recipe';
 import { Router } from '@angular/router';
+import { ProfileService } from '../demo/service/profile.service';
+import { Profile } from '../model/profile';
 
 @Component({
     selector: 'app-topbar',
@@ -11,15 +13,54 @@ import { Router } from '@angular/router';
 export class AppTopBarComponent {
     recipes: Recipe[] = [];
     login: boolean = false;
-
+    profile: Profile = {
+        id: 0,
+        name: '',
+        imageFile: '',
+    };
+    userImage: any;
+    items: MenuItem[] | undefined;
     @ViewChild('menubutton') menuButton!: ElementRef;
 
     @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService, private router: Router) {}
+    constructor(
+        public layoutService: LayoutService,
+        private router: Router,
+        private profileService: ProfileService
+    ) {}
     ngOnInit() {
+        this.profileService.getMe().subscribe({
+            next: (res: any) => {
+                this.profile.id = res?.data?.user?.id;
+                this.profile.name = res?.data?.user?.name;
+                this.userImage = res?.data?.image;
+                this.login = true;
+            },
+        });
+        this.items = [
+            {
+                label: 'Profile',
+                items: [
+                    {
+                        label: 'Profile',
+                        icon: 'pi pi-user',
+                        command: () => {
+                            this.router.navigate(['user/profile']);
+                        },
+                    },
+                    {
+                        label: 'Sign out',
+                        icon: 'pi pi-sign-out',
+                        command: () => {
+                            this.logout();
+                        },
+                    },
+                ],
+            },
+        ];
     }
     searchRecipes(name: string) {
         this.router.navigate(['search', { searchTerm: name }]);
@@ -27,5 +68,15 @@ export class AppTopBarComponent {
 
     myRecipes() {
         this.router.navigate(['./myRecipe']);
+    }
+    logout() {
+        this.profileService.logout().subscribe({
+            next: () => {
+                this.router.navigate(['./auth/login']);
+            },
+            error: (err) => {
+                this.router.navigate(['./auth/login']);
+            },
+        });
     }
 }
